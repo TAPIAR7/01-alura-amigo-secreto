24ddee98464ae563793128f78be57f6afd758c80
TAPIAR7 <rtapiatec@gmail.com>
2025-01-27 15:28:16 -0600

    Agregando amigo a la vista y la pagina visual

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amgigos = [];
+
+function agregarAmigo() {
+    let nombre = obtenerElementoPorID('amigo');
+    if (!verificarCampoVacio(nombre)) {
+        amgigos.push({nombre});
+        agregarLista(nombre, 'listaAmigos');
+    } else {
+        alert('El campo no puede estar vacío');
+    }
+}
+
+function obtenerElementoPorID(id){
+    return document.getElementById(id).value;
+}
+
+function verificarCampoVacio(valorDeEntrada){
+    return valorDeEntrada === '';
+}
+
+function agregarLista(nombre, listID){
+    let myList = document.getElementById(listID);
+    myList.innerHTML += "<li>" + nombre + "</li>"; 
+}
