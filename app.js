e7289807d23caec96ab502e89bdb29e30443bf43
TAPIAR7 <rtapiatec@gmail.com>
2025-02-01 15:03:28 -0600

    unicamente permitiendo nombres unicos

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amgigos = [];
+let amigos = [];
 
 function agregarAmigo() {
     let nombre = obtenerElementoPorID('amigo');
-    if (!verificarCampoVacio(nombre)) {
-        amgigos.push(nombre);
-        agregarListaLi(nombre, 'listaAmigos');
-    } else {
+    if (verificarCampoVacio(nombre)) {
         alert('El campo no puede estar vacío');
+    } else if (verificarEntradaNumerica(nombre)){
+        alert('El campo no puede ser numérico');
+    } else if (verificarNombreDuplicado(nombre)) {
+        alert('El amigo ya fue agregado');
+    
+    } else if (verificarEspaciosEnBlanco(nombre)) {
+        alert('El campo no puede ser solo espacios en blanco');
+    }
+    else {
+        amigos.push(nombre);
+        agregarListaLi(nombre, 'listaAmigos');
     }
     limpiarCampoEntrada('amigo');
 }
@@ -20,9 +28,23 @@ function verificarCampoVacio(valorDeEntrada){
     return valorDeEntrada === '';
 }
 
+function verificarEntradaNumerica(valorDeEntrada){
+    const numeroRegex = /^-?\d+(\.\d+)?$/; // Expresión regular para verificar si es un número incluidos negativos
+    return numeroRegex.test(valorDeEntrada);
+}
+
+function verificarNombreDuplicado(valorDeEntrada){
+    let amigosMinuscula = amigos.map(amigo => amigo.toLowerCase());
+    return amigosMinuscula.includes(valorDeEntrada.toLowerCase());
+}
+
+function verificarEspaciosEnBlanco(valorDeEntrada){
+    return valorDeEntrada.trim().length === 0;
+}
+
 function agregarListaLi(texto, listID){
-    let myList = document.getElementById(listID);
-    myList.innerHTML += "<li>" + texto + "</li>"; 
+    let listaLi = document.getElementById(listID);
+    listaLi.innerHTML += "<li>" + texto + "</li>"; 
 }
 
 function limpiarCampoEntrada(id){
@@ -33,11 +55,11 @@ function limpiarListaLi(id){
 }
 
 function sortearAmigo(){
-    let cantidadAmigos = amgigos.length;
+    let cantidadAmigos = amigos.length;
 
     if (cantidadAmigos > 0) {
         let amigoSeleccionado = Math.floor(Math.random() * cantidadAmigos);
-        let amigo = amgigos[amigoSeleccionado];
+        let amigo = amigos[amigoSeleccionado];
         limpiarCampoEntrada('amigo');
         limpiarListaLi('listaAmigos')
         agregarListaLi(`El amigo secreto sorteado es: ${amigo}`, 'resultado');    
